598e33d61432f711dfa664ccb78c1b9f519e7b09
DanielMGDev <daniel.moralesg.dev@gmail.com>
2025-11-17 17:57:11 -0500

    cambie españa por colombia

--- components/footer.tsx
+++ components/footer.tsx
@@ -75,7 +75,7 @@ export function Footer() {
               </div>
               <div className="flex items-center gap-2">
                 <MapPin className="h-4 w-4" />
-                <span>Madrid, España</span>
+                <span>Colombia, Politecnico</span>
               </div>
             </div>
           </div>
